6a22292d5aae9c3a66079ef3f195589626252700
Hasan Siam <hasansiam4545@gmail.com>
2021-05-02 13:44:03 +0600

    form added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
 import MyNavbar from '../Navbar/MyNavbar'
-
+import emailjs from 'emailjs-com';
 const Contact = () => {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_01n3d3n', e.target)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
     return (
         <div className="text-center bg-dark text-white">
             <MyNavbar></MyNavbar>
             <h3 className="p-3">Contact With Me</h3>
-            <form name="contact" method="POST" data-netlify="true">
+            <form onSubmit={sendEmail}>
                 
-            <input className="p-2 rounded" type="text" name="name" id="name" placeholder="Your Name" /> <br/> <br/>
-            <input className="p-2 rounded" type="email" name="email" id="email" placeholder="Your Email" /> <br/> <br/>
+            <input className="p-2 rounded" type="text" name="name"  placeholder="Your Name" /> <br/> <br/>
+            <input className="p-2 rounded" type="email" name="email"  placeholder="Your Email" /> <br/> <br/> 
+            <input className="p-2 rounded" type="text" name="subject"  placeholder="subject" /> <br/> <br/> 
             <textarea className="p-2 mb-4 rounded" name="message" id="message" cols="40" rows="7" placeholder="Your Message"></textarea> <br/> 
-            <button className="btn btn-primary" type="submit">SEND</button>
+            <input className="btn btn-primary" type="submit" value="SEND" />
             </form>
             <Footer></Footer>
         </div>
